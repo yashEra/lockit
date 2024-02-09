306f469728140584a98e23a9c6fd4eee0518afbb
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2024-02-10 02:45:21 +0530

    Update

--- src/components/title/MainTitle.jsx
+++ src/components/title/MainTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainTitle = (title) => {
     return (
-      <div className="flex flex-col items-center pb-8 pt-16">
+      <div className="flex flex-col items-center pb-8 pt-16 px-6">
         <h1 className="text-[36px] font-bold text-[#00A5AF] pb-2 text-center">{title.main}</h1>
         <div className='w-[145px]'>
         <hr className="border-[2px] border-[#00A5AF]"/>
